996caafab86481144fb2a1f5da34adff777aad29
reuel.rai <reuelrai2001@gmail.com>
2026-06-21 23:52:26 +0545

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <h2>This is a simple heading.</h2>
       <h3>This is a third-level heading.</h3>
+      <h4>This is a fourth-level heading.</h4>
     </>
   );
 }
